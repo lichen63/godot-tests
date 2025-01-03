1e6cdd35c759fe3500c95ad9dcdc720b5c50ca95
Lichen Liu <liu.lichen63@gmail.com>
2025-01-03 15:32:28 +0800

    update image blit_rect

--- image-blit-rect/main.gd
+++ image-blit-rect/main.gd
@@ -7,9 +7,7 @@ func _ready() -> void:
     var src_texture = texture_src.texture
     var image = src_texture.get_image()
     var rect = Rect2(5, 5, 50, 50)
-    var cropped_image = Image.new()
-    cropped_image.create(rect.size.x, rect.size.y, false, image.get_format())
+    var cropped_image = Image.create_empty(rect.size.x, rect.size.y, false, image.get_format())
     cropped_image.blit_rect(image, rect, Vector2.ZERO)
-    var cropped_texture = ImageTexture.new()
-    cropped_texture.create_from_image(cropped_image)
+    var cropped_texture = ImageTexture.create_from_image(cropped_image)
     texture_dst.texture = cropped_texture
