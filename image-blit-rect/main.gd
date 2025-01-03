911f7919f91c1307a7fd1507f7a93f76b1050e1f
Lichen Liu <liu.lichen63@gmail.com>
2025-01-03 14:38:18 +0800

    add image-blit-rect

---
+++ image-blit-rect/main.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var texture_src: TextureRect = $TextureRectSrc
+@onready var texture_dst: TextureRect = $TextureRectDst
+
+func _ready() -> void:
+    var src_texture = texture_src.texture
+    var image = src_texture.get_image()
+    var rect = Rect2(5, 5, 50, 50)
+    var cropped_image = Image.new()
+    cropped_image.create(rect.size.x, rect.size.y, false, image.get_format())
+    cropped_image.blit_rect(image, rect, Vector2.ZERO)
+    var cropped_texture = ImageTexture.new()
+    cropped_texture.create_from_image(cropped_image)
+    texture_dst.texture = cropped_texture
